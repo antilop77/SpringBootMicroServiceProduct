26512df57a2feadf8d053a06fb56a46f96477032
inf_idris.boz <idris.boz@infinidium.com.tr>
2022-09-13 11:52:49 +0300

    init

---
+++ src/main/java/com/infinidium/oracle/product/SpringBootMicroserviceProductApplication.java
@@ -0,0 +1,13 @@
+package com.infinidium.oracle.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infinidium/oracle/product/SpringBootMicroserviceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinidium.oracle.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
